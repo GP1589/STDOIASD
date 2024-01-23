4dc78a9eae7a2ddf4735a1bc256cecae701c7b33
Christian Castro <krisfb51@gmail.com>
2024-01-23 16:04:29 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace petbnb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
